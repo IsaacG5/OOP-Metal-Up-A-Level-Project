e06c497c93434add6c8ba5338083f0ec80116c19
Isaac Glynn <isaacglynn@online.sch.im>
2022-07-20 20:50:42 +0100

    Exercise 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.Canvas.Location = new System.Drawing.Point(190, 12);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(598, 426);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Canvas);
             this.Name = "Form1";
             this.Text = "OOPDraw";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,17 @@ namespace OOP_Metal_Up_A_Level_Project
         public Form1()
         {
             InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        Pen currentPen = new Pen(Color.Black);
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            Point a = new Point(20, 30);
+            Point b = new Point(400, 500);
+            gr.DrawLine(currentPen, a, b);
         }
     }
 }
