2076cb970697bcb52d34e83b2ea8d2044562f15f
Isaac Glynn <isaacglynn@online.sch.im>
2022-07-20 21:00:49 +0100

    Exercise 4

--- Form1.cs
+++ Form1.cs
@@ -22,23 +22,30 @@ namespace OOP_Metal_Up_A_Level_Project
         bool dragging = false;
         Point startOfDrag = Point.Empty;
         Point lastMousePosition = Point.Empty;
+        List<Line> lines = new List<Line>();
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
             Graphics gr = e.Graphics;
-            gr.DrawLine(currentPen, startOfDrag, lastMousePosition);
+            foreach (Line line in lines)
+            { 
+                line.Draw(gr);
+            }
         }
 
         private void Canvas_MouseDown(object sender, MouseEventArgs e)
         {
             dragging = true;
             startOfDrag = lastMousePosition = e.Location;
+            lines.Add(new Line(currentPen, e.X, e.Y));
         }
 
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (dragging)
             {
+                Line currentLine = lines.Last();
+                currentLine.GrowTo(e.X, e.Y);
                 lastMousePosition = e.Location;
                 Refresh(); 
             }
